6443d473f66f70409f8934c232d4c71094e106b0
ANelson44 <145131926+ANelson44@users.noreply.github.com>
2024-01-17 13:00:43 -0500

    updated server.js

--- server.js
+++ server.js
@@ -10,3 +10,34 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+      maxAge: process.env.COOKIE_MAX_AGE || 300000, // Default to 5 minutes if not set in .env
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
