c2d2b04504fc2acd561dab0e669cb682eee40dd9
ANelson44 <145131926+ANelson44@users.noreply.github.com>
2024-01-21 11:40:13 -0500

    fixed various errors to start server. added basic html for testing

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers: require("./utils/helpers") });
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 const sess = {
     secret: process.env.SESSION_SECRET,
     cookie: {
-      maxAge: process.env.COOKIE_MAX_AGE || 300000, // Default to 5 minutes if not set in .env
+      maxAge: Number(process.env.COOKIE_MAX_AGE || 300000), // Default to 5 minutes if not set in .env
       httpOnly: true,
       secure: false,
       sameSite: 'strict',
