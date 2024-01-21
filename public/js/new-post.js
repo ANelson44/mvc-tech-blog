c62615ab80c8490266978d3311eb6b04c0d2e865
ANelson44 <145131926+ANelson44@users.noreply.github.com>
2024-01-21 13:01:11 -0500

    added login and new-post js code. updated multiple handlebar files

---
+++ public/js/new-post.js
@@ -0,0 +1,26 @@
+const newBlogPostFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#title-new-blog-post').value.trim();
+    const content = document.querySelector('#content-new-blog-post').value.trim();
+  
+    if (title && content) {
+      const response = await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({ title, content }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard'); // When successful, load the dashboard page
+      } else {
+        alert('Failed to create a new post.'); // When unsuccessful, show alert
+      }
+    }
+  };
+  
+  // Event listeners
+  const newBlogPostForm = document.querySelector('.new-blog-post-form');
+  if (newBlogPostForm) {
+    newBlogPostForm.addEventListener('submit', newBlogPostFormHandler);
+  }
